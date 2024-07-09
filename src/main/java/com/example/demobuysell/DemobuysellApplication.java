e42e2b83ad8bebc6d86e28b3e54764bcb4bf1c44
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-07-09 14:30:12 +0300

    init java whit help spring start io

---
+++ src/main/java/com/example/demobuysell/DemobuysellApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobuysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobuysellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobuysellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobuysell/DemobuysellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobuysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobuysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
